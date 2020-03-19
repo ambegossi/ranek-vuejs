6051ee7f97af76fa7b3a757313f4c699cfd70708
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-19 17:41:42 -0300

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb', 'prettier'],
   parserOptions: {
     parser: 'babel-eslint',
   },
+  plugins: ['prettier'],
   rules: {
+    'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
