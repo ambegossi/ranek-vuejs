1dad7bb70fc93819e435a0886cb4a3805c14bcac
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-20 11:42:56 -0300

    Stylized inputs

--- src/components/ProductsSearch.vue
+++ src/components/ProductsSearch.vue
@@ -1,6 +1,12 @@
 <template>
   <form>
-    <input name="search" id="search" type="text" v-model="search" />
+    <input
+      name="search"
+      id="search"
+      type="text"
+      v-model="search"
+      placeholder="Buscar..."
+    />
     <input
       type="submit"
       id="submit"
@@ -25,4 +31,34 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#search {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#search:focus,
+#search:hover {
+  transform: scale(1.1);
+}
+
+#submit {
+  width: 62px;
+  height: 62px;
+  background: url('../assets/search.svg') no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  box-shadow: none;
+}
+</style>
