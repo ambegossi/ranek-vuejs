8db89e1d3e12dbb1b2fa4af9231b8bd6b7434c50
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-23 15:30:20 -0300

    Added User Update

--- src/views/user/UserEdit.vue
+++ src/views/user/UserEdit.vue
@@ -1,9 +1,34 @@
 <template>
-  <p>Editar Usuário</p>
+  <section>
+    <UserForm>
+      <button class="btn" @click.prevent="updateUser">Atualizar Usuário</button>
+    </UserForm>
+  </section>
 </template>
 
 <script>
-export default {};
+import UserForm from '../../components/UserForm.vue';
+import api from '../../services/api';
+
+export default {
+  name: 'UserEdit',
+  components: {
+    UserForm,
+  },
+  methods: {
+    updateUser() {
+      api
+        .put(`/user/${this.$store.state.user.id}`, this.$store.state.user)
+        .then(() => {
+          this.$store.dispatch('getUser');
+          this.$router.push({ name: 'User' });
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
