2f6af1368a9e3f11c70bf93dfb054e1572741d4f
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-23 14:17:02 -0300

    Added Product Add

--- src/components/ProductAdd.vue
+++ src/components/ProductAdd.vue
@@ -1,11 +1,64 @@
 <template>
-  <p>Produto Adicionar</p>
+  <form class="add-product">
+    <label for="name">Nome</label>
+    <input id="name" name="name" type="text" v-model="product.name" />
+    <label for="price">Preço (R$)</label>
+    <input id="price" name="price" type="number" v-model="product.price" />
+    <label for="photos">Fotos</label>
+    <input id="photos" name="photos" type="file" ref="photos" />
+    <label for="description">Descrição</label>
+    <textarea
+      id="description"
+      name="description"
+      v-model="product.description"
+    ></textarea>
+    <input
+      class="btn"
+      type="button"
+      value="Adicionar Produto"
+      @click.prevent="addProduct"
+    />
+  </form>
 </template>
 
 <script>
+import api from '../services/api';
+
 export default {
   name: 'ProductAdd',
+  data() {
+    return {
+      product: {
+        name: '',
+        price: '',
+        description: '',
+        photos: null,
+      },
+    };
+  },
+  methods: {
+    formatProduct() {
+      this.product.user_id = this.$store.state.user.id;
+    },
+    addProduct() {
+      this.formatProduct();
+      api.post('/product', this.product).then(() => {
+        this.$store.dispatch('getUserProducts');
+      });
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.add-product {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  align-items: center;
+  margin-bottom: 60px;
+}
+
+.btn {
+  grid-column: 2;
+}
+</style>
--- src/views/user/UserProducts.vue
+++ src/views/user/UserProducts.vue
@@ -47,4 +47,15 @@ export default {
 h2 {
   margin-bottom: 20px;
 }
+
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translate3d(20px, 0, 0);
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.3s;
+}
 </style>
