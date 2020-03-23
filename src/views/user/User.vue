94c1851b8a45d661e9317ff092e754c8c453c7bb
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-23 10:43:02 -0300

    Added User style

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -15,7 +15,7 @@
           <router-link :to="{ name: 'User-Edit' }">Editar Usuário</router-link>
         </li>
         <li>
-          <button @click.prevent="signOut">Deslogar</button>
+          <button @click="signOut">Deslogar</button>
         </li>
       </ul>
     </nav>
@@ -37,4 +37,46 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.user {
+  display: grid;
+  grid-template-columns: minmax(140px, 200px) 1fr;
+  max-width: 900px;
+  margin: 40px auto;
+  grid-gap: 30px;
+  padding: 20px;
+}
+
+@media screen and (max-width: 500px) {
+  .user {
+    grid-template-columns: 1fr;
+    margin: 0px auto;
+  }
+}
+
+.sidenav a,
+.sidenav button {
+  padding: 10px;
+  display: block;
+  background: #f4f7fc;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.sidenav a.router-link-exact-active,
+.sidenav a:hover,
+.sidenav button:hover {
+  background: #87f;
+  color: #fff;
+}
+
+.sidenav button {
+  border: none;
+  width: 100%;
+  font-size: 1rem;
+  text-align: left;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  cursor: pointer;
+  color: #345;
+}
+</style>
